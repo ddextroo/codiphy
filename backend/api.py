5ba2768ff30dfd8c73455f4e91b21c95b2625cc6
ddextroo <dexteringuito@gmail.com>
2023-12-26 10:07:58 +0800

    bkc

--- backend/api.py
+++ backend/api.py
@@ -1,27 +1,56 @@
-from flask import Flask
-from flask_cors import CORS
-
+import g4f
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/data')
-def get_time():
-    # Returning an api for showing in  reactjs
-    return {
-        'Name':"dexter",
-        "Age":"20",
-        "programming":"python"
-        }
+@app.route("/api/quiz", methods=["POST"])
+def generate_quiz():
+    g4f.debug.logging = False  # Enable debug logging
+    g4f.debug.check_version = False  # Disable automatic version checking
+    data = request.get_json()
+    topic = data.get("topic")
+    language = data.get("language")
+    items = int(data.get("items"))
+    query_type = data.get("type")
 
-@app.route('/profile')
-def my_profile():
-    response_body = {
-        "name": "Dextro",
-        "about" :"Hello!"
-    }
+    if query_type == "basic":
+        query = f"""Generate a quiz about this topic: {topic},
+        'Maximum of: {items} items,
+        'adhere to the following rules:\n'
+        '1. Must be multiple choices
+        '2. The format will be 
+        Question
+        (a) a
+        (b) b
+        (c) c
+        (d) d
+        Answer: answer,
+        The answer must be letter only
+        'Output the result in "json" format:\n'
+        """
+    else:
+        query = f"""Generate a quiz about this topic: {topic},
+        'Maximum of: ({items}) items,
+        'adhere to the following rules:\n'
+        '1. The items must be tracing of the sample {language} program
+        '2. The format will be 
+        Question
+        Answer: answer,
+        '3. Avoid using the ```{language} ``` code block notation.\n'
+        'Output the result in ".json" format:\n'
+        """
 
-    return response_body
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.gpt_35_long,
+        messages=[
+            {
+                "role": "user",
+                "content": query,
+            }
+        ],
+    )
+    return jsonify({'response': response})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
