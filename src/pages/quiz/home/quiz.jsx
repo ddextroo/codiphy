49e45ceffcc4747d88b1a3bdb3370771590f60af
ddextroo <dexteringuito@gmail.com>
2023-12-27 16:33:52 +0800

    ew

--- src/pages/quiz/home/quiz.jsx
+++ src/pages/quiz/home/quiz.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../../../routes/mainRoutes";
 import { useLogout } from "./../../../hooks/useLogout";
-import { Navigate } from "react-router-dom";
+import { Navigate, Link } from "react-router-dom";
 function Quiz() {
   const { user, authIsReady } = useContext(AuthContext);
   const { logout } = useLogout();
@@ -16,9 +16,12 @@ function Quiz() {
           <button className="btn login-btn" onClick={logout}>
             Logout
           </button>
+          <button className="w-32 py-4 bg-green-800 flex flex-row justify-center rounded-xl items-center space-x-3">
+            <Link to="/start">Take quiz</Link>
+          </button>
         </div>
       ) : (
-        <Navigate to='/'/>
+        <Navigate to="/" />
       )}
     </div>
   ) : (
