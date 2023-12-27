a6f708126a13bf276598f9a4852da653c2d15dc2
ddextroo <dexteringuito@gmail.com>
2023-12-27 16:50:54 +0800

    wwe

--- src/Error.jsx
+++ src/Error.jsx
@@ -7,7 +7,7 @@ const Error = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-primaryLight p-4 font-montserrat">
       <div className="text-center max-w-2xl w-full">
-        <div className="text-4xl text-black mb-8">Oops! Something went wrong.</div>
+        <div className="text-4xl text-black mb-8 font-bold">Oops! Something went wrong.</div>
         <div className="text-lg text-gray mb-10">
           We apologize, but it seems there's been an error. Please try refreshing the page or contact support.
         </div>
