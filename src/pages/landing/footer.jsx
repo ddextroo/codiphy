9186ffe8a9ff8dc72d8dd0e21686baae170e3ae0
Jamexxsxs <jomarycoquilla465@gmail.com>
2023-12-28 18:57:39 +0800

    ew

--- src/pages/landing/footer.jsx
+++ src/pages/landing/footer.jsx
@@ -3,10 +3,12 @@ import { RiFacebookCircleFill } from "react-icons/ri";
 import { RiDiscordFill } from "react-icons/ri";
 import { RiInstagramFill } from "react-icons/ri";
 import Privacy from "./footer/privacy";
+import About from "./footer/aboutus";
 import { useState } from "react";
 
 function footer() {
   const [showPrivacy, setshowPrivacy] = useState(false);
+  const [showAbout, setshowAbout] = useState(true);
 //goods na, kamo nalang bahala sa contents ana. kibaw namos design tong sample naa sa generator
 // adto nalang mo edit sa privacy
 // ako nani i off?
@@ -17,6 +19,13 @@ function footer() {
   const closePrivacy = () => {
     setshowPrivacy(false);
   };
+  const openAbout = () => {
+    setshowAbout(true);
+  };
+
+  const closeAbout = () => {
+    setshowAbout(false);
+  };
   return (
     <div className="bg-primaryLight rounded-xl">
       <div className="flex flex-row justify-center space-x-5">
@@ -25,7 +34,7 @@ function footer() {
         <RiInstagramFill size={30} color="gray" />
       </div>
       <div className="flex flex-row justify-center space-x-5 mt-3">
-        <div className="text-md font-medium underline text-gray cursor-pointer">
+        <div className="text-md font-medium underline text-gray cursor-pointer"  onClick={openAbout}>
           About
         </div>
         <button className="text-md font-medium underline text-gray cursor-pointer" onClick={openPrivacy}>
@@ -40,6 +49,7 @@ function footer() {
       </div>
       <div className="text-gray text-sm my-3">Crafted By: DEV</div>
       {showPrivacy && <Privacy onClose={closePrivacy} />}
+      {showAbout && <About onClose={closeAbout} />}
     </div>
   );
 }
---
+++ src/pages/landing/footer/aboutus.jsx
@@ -0,0 +1,143 @@
+import { IoCloseCircle } from "react-icons/io5";
+import React from "react";
+
+const About = ({ onClose }) => {
+  return (
+    <div className="modal-overlay justify-center items-center flex fixed inset-0 z-50 bg-black bg-opacity-50 rounded-xl">
+      <div className="modal justify-center items-center flex flex-col overflow-x-hidden overflow-y-auto relative w-auto my-6 mx-auto">
+        <div className="modal-content p-5 backdrop-filter divide-y divide-gray space-y-3 bg-primaryLight rounded-xl max-h-full h-full font-poppins overflow-auto overflow-x-hidden ml-20 mr-20 max-w-3xl">
+          <div className="flex flex-row justify-between items-center mt-5 mb-7">
+            <div className="text-3xl font-semibold">About us</div>
+            <IoCloseCircle
+              size={25}
+              color="gray"
+              onClick={onClose}
+              className=" cursor-pointer"
+            />
+          </div>
+          <div className="overflow-y-auto h-[30rem] py-5 text-left">
+            <div className="text-xl font-semibold text-center">
+              About the App
+            </div>
+            <div className="text-md md:text-lg bg-opacity-0 py-5">
+              <div className="mb-3 text-justify px-5">
+                Codiphy is a cutting-edge educational platform revolutionizing
+                programming learning through AI-generated challenges,
+                personalized learning paths, and a vibrant community. The
+                platform's dynamic interface ensures a seamless and engaging
+                experience, while its AI-driven question formulation tailors
+                challenges to individual skill levels. Codiphy fosters a
+                supportive community where learners can collaborate, share
+                knowledge, and celebrate successes. The personalized learning
+                paths cater to each user's unique strengths and preferences,
+                allowing for flexible and targeted skill development. In
+                essence, Codiphy combines innovative technology with a
+                collaborative community to redefine how individuals enhance
+                their coding skills, making programming proficiency accessible
+                and enjoyable for learners of all levels.
+              </div>
+              <div className="text-md md:text-lg bg-opacity-0 py-5"></div>
+            </div>
+            <div className="text-xl font-semibold text-center">Developers</div>
+            <div className="grid grid-cols-2 gap-y-20 gap-2 md:grid-cols-3 place-items-center mt-10">
+            <div className="flex flex-col gap-y-6 justify-start md:justify-center items-center space-x-2 w-full">
+            <div className="w-28 h-28 rounded-full bg-colorAccent"></div>
+            <div className="text-lg font-medium md:text-2xl">
+              Dexter G. Inguito
+            </div>
+            <a
+              href="https://dextrolio.netlify.app/"
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              <button className="w-32 py-2 text-md md:text-sm rounded-xl font-medium bg-colorAccent text-primaryLight px-5">
+                View profile
+              </button>
+            </a>
+            </div>
+
+
+              <div className="flex flex-col gap-y-6 justify-start md:justify-center items-center space-x-2 w-full">
+                <div className=" w-28 h-28 rounded-full bg-colorAccent"></div>
+                <div className="text-lg font-medium md:2xl">Aivy C. Unabia</div>
+                <a
+                  href="https://aesthethyve-unabia-portfolio.netlify.app/"
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <button className="w-32 py-2 text-md md:text-sm rounded-xl font-medium bg-colorAccent text-primaryLight px-5">
+                    View profile
+                  </button>
+                </a>
+              </div>
+
+              <div className="flex flex-col gap-y-6 justify-start md:justify-center items-center space-x-2 w-full">
+                <div className=" w-28 h-28 rounded-full bg-colorAccent"></div>
+                <div className="text-lg font-medium md:2xl">
+                  Jessel Ann Piamonte
+                </div>
+                <a
+                  href="https://piamontejess.github.io/jesselann/?fbclid=IwAR1iZG96rcZ83WmrHdvyhTJZjYl4VcpAcgPMfCdRQQqUvvO7spG9mM_RcSY"
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <button className="w-32 py-2 text-md md:text-sm rounded-xl font-medium bg-colorAccent text-primaryLight px-5">
+                    View profile
+                  </button>
+                </a>
+              </div>
+
+              <div className="flex flex-col gap-y-6 justify-start md:justify-center items-center space-x-2 w-full">
+                <div className=" w-28 h-28 rounded-full bg-colorAccent"></div>
+                <div className="text-lg font-medium md:2xl">
+                  Jane Leslie Fiel
+                </div>
+                <a
+                  href=""
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <button className="w-32 py-2 text-md md:text-sm rounded-xl font-medium bg-colorAccent text-primaryLight px-5">
+                    View profile
+                  </button>
+                </a>
+              </div>
+              <div className="flex flex-col gap-y-6 justify-start md:justify-center items-center space-x-2 w-full ">
+                <div className=" w-28 h-28 rounded-full bg-colorAccent"></div>
+                <div className="text-lg font-medium md:2xl">
+                  Kenny Jay O. Largo
+                </div>
+                <a
+                  href="https://kennyjay-portfolio.netlify.app/"
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <button className="w-32 py-2 text-md md:text-sm rounded-xl font-medium bg-colorAccent text-primaryLight px-5">
+                    View profile
+                  </button>
+                </a>
+              </div>
+              <div className="flex flex-col gap-y-6 justify-start md:justify-center items-center space-x-2 w-full ">
+                <div className=" w-28 h-28 rounded-full bg-colorAccent"></div>
+                <div className="text-lg font-medium md:2xl">
+                  Lourd Jonas Torrejos
+                </div>
+                <a
+                  href=""
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <button className="w-32 py-2 text-md md:text-sm rounded-xl font-medium bg-colorAccent text-primaryLight px-5">
+                    View profile
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
