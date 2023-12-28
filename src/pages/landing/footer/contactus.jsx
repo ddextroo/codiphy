18f43e0f101a8b431ce947e0561d0ce9186d0639
Jamexxsxs <jomarycoquilla465@gmail.com>
2023-12-28 15:53:46 +0800

    434

---
+++ src/pages/landing/footer/contactus.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function contactus() {
+
+    return (
+      <div className="modal-overlay justify-center items-center flex fixed inset-0 z-50 bg-black bg-opacity-50 rounded-xl">
+        <div className="modal justify-center items-center flex flex-col overflow-x-hidden overflow-y-auto relative w-auto my-6 mx-auto">
+          <div className="modal-content p-5 backdrop-filter divide-y divide-gray space-y-3 bg-primaryLight rounded-xl max-h-full h-full font-poppins overflow-auto overflow-x-hidden ml-20 mr-20 max-w-3xl">
+
+            
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+
+export default contactus
