bc648c510e46b76dc2c5b1108974089bc316c780
Jamexxsxs <jomarycoquilla465@gmail.com>
2024-01-10 15:55:56 +0800

    ken

--- src/pages/landing/footer/aboutus.jsx
+++ src/pages/landing/footer/aboutus.jsx
@@ -1,5 +1,12 @@
 import { IoCloseCircle } from "react-icons/io5";
 import React from "react";
+import dexter from "./../../../assets/Members/d.jpg";
+import kenny from "./../../../assets/Members/20220807_153551-01.jpeg";
+import lj from "./../../../assets/Members/lj.jpg";
+import aivy from "./../../../assets/Members/aivyh.jpg";
+import jessel from "./../../../assets/Members/jesil.jpg"
+import leslie from "./../../../assets/Members/leslie.jpeg"
+
 
 const About = ({ onClose }) => {
   return (
@@ -39,9 +46,16 @@ const About = ({ onClose }) => {
               <div className="text-md md:text-lg bg-opacity-0 py-5"></div>
             </div>
             <div className="text-xl font-semibold text-center">Developers</div>
-            <div className="grid grid-cols-2 gap-y-20 gap-2 md:grid-cols-3 place-items-center mt-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 md:gap-y-20 gap-2 lg:grid-cols-3 place-items-center mt-10">
               <div className="flex flex-col gap-y-6 justify-start md:justify-center items-center space-x-2 w-full">
-                <div className="w-28 h-28 rounded-full bg-colorAccent"></div>
+                <div
+                  className="w-28 h-28 rounded-full"
+                  style={{
+                    backgroundImage: `url(${dexter})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                ></div>
                 <div className="text-lg font-medium md:2xl">
                   Dexter G. Inguito
                 </div>
@@ -56,7 +70,14 @@ const About = ({ onClose }) => {
                 </a>
               </div>
               <div className="flex flex-col gap-y-6 justify-start md:justify-center items-center space-x-2 w-full">
-                <div className=" w-28 h-28 rounded-full bg-colorAccent"></div>
+              <div
+                  className="w-28 h-28 rounded-full"
+                  style={{
+                    backgroundImage: `url(${aivy})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                ></div>
                 <div className="text-lg font-medium md:2xl">Aivy C. Unabia</div>
                 <a
                   href="https://aesthethyve-unabia-portfolio.netlify.app/"
@@ -70,7 +91,14 @@ const About = ({ onClose }) => {
               </div>
 
               <div className="flex flex-col gap-y-6 justify-start md:justify-center items-center space-x-2 w-full">
-                <div className=" w-28 h-28 rounded-full bg-colorAccent"></div>
+              <div
+                  className="w-28 h-28 rounded-full"
+                  style={{
+                    backgroundImage: `url(${jessel})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                ></div>
                 <div className="text-lg font-medium md:2xl">
                   Jessel Ann Piamonte
                 </div>
@@ -86,7 +114,15 @@ const About = ({ onClose }) => {
               </div>
 
               <div className="flex flex-col gap-y-6 justify-start md:justify-center items-center space-x-2 w-full">
-                <div className=" w-28 h-28 rounded-full bg-colorAccent"></div>
+              <div
+                  className="w-28 h-28 rounded-full"
+                  style={{
+                    backgroundImage: `url(${leslie})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                ></div>
+
                 <div className="text-lg font-medium md:2xl">
                   Jane Leslie Fiel
                 </div>
@@ -97,10 +133,18 @@ const About = ({ onClose }) => {
                 </a>
               </div>
               <div className="flex flex-col gap-y-6 justify-start md:justify-center items-center space-x-2 w-full ">
-                <div className=" w-28 h-28 rounded-full bg-colorAccent"></div>
+              <div
+                  className="w-28 h-28 rounded-full"
+                  style={{
+                    backgroundImage: `url(${kenny})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                ></div>
                 <div className="text-lg font-medium md:2xl">
                   Kenny Jay O. Largo
                 </div>
+                
                 <a
                   href="https://kennyjay-portfolio.netlify.app/"
                   target="_blank"
@@ -112,7 +156,14 @@ const About = ({ onClose }) => {
                 </a>
               </div>
               <div className="flex flex-col gap-y-6 justify-start md:justify-center items-center space-x-2 w-full ">
-                <div className=" w-28 h-28 rounded-full bg-colorAccent"></div>
+              <div
+                  className="w-28 h-28 rounded-full"
+                  style={{
+                    backgroundImage: `url(${lj})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                ></div>
                 <div className="text-lg font-medium md:2xl">
                   Lourd Jonas Torrejos
                 </div>
